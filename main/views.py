79b44491ddf1362fbb8509300fdf55ad6055aaff
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-11 18:31:40 -0400

    Update main/views.py - add about view - add contact view - add projects view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def projects(request):
+    return render(request, 'main/projects.html')
