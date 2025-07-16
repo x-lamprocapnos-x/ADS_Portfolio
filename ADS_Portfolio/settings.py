b1e7c93b9f4aebaf95a8136fde5ce3566058d3b5
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-16 19:32:29 -0400

    Update settings.py - install dotenv - import load_dotenv - load env variables for email form configuration

--- ADS_Portfolio/settings.py
+++ ADS_Portfolio/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +136,11 @@ TAILWIND_APP_NAME = 'theme'
 
 # Node/NPM config 
 NPM_BIN_PATH = 'C:/Program Files/nodejs/npm.cmd'
+
+# Email config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
