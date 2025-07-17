63634019341514fa15818fa246c909b5063f7539
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-17 19:48:04 -0400

    Update urls.py - add app name for a consistent namespace habit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
