49f952e92ec6dc1f611bcfd25cfe511699549342
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-17 19:56:13 -0400

    update root urls.py - add namespace in url path

--- ADS_Portfolio/urls.py
+++ ADS_Portfolio/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__reload__/', include('django_browser_reload.urls')),
-    path('', include('main.urls'))
+    path('', include('main.urls', namespace='main'))
 ]
