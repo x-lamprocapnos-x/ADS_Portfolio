46bad58f03376dc767a6a284ac7a4217b4df24d8
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-20 16:32:30 -0400

    Add Project model for Projects page, implement slug for project_detail page and account for links to github, and live site

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
+# main/models.py
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='project_images/')
+    description = models.TextField()
+    case_study = models.TextField() # Will be URL when done
+    github_url = models.URLField(blank=True, null=True)
+    live_url = models.URLField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('main:project_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Project, self).save(*args, **kwargs)
