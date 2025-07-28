2459bad9f50bd84a4bce030f2c23f0ebd16dce2c
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-28 16:20:07 -0400

    add slug url path

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('projects/', views.projects, name='projects'),
+    path('projects/<slug:slug>/', views.project_detail, name='project_detail')
 ]
