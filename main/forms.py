aa8b9ce1e88fad355322ace72ca108a54781f65b
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-07-30 20:26:21 -0400

    Update forms.py - add django_recaptcha logic for security - update class for tailwind styling

--- main/forms.py
+++ main/forms.py
@@ -1,17 +1,19 @@
 # main/forms.py
 
 from django import forms
+from django_recaptcha.fields import ReCaptchaField
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'border rounded w-full py-2 px-3',
+        'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500',
         'placeholder': 'Your Name'
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'border rounded w-full py-2 px-3',
+        'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500',
         'placeholder': "Your Email"
     }))
     message = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'border rounded w-full py-2 px-3',
+        'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500',
         'placeholder': 'Your Message'
     }))
+    captcha = ReCaptchaField()
