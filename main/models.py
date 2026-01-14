e11035e669f81ea267803295c4a35e3c6e514707
Lamprocapnos <shoben2799@gmail.com>
2026-01-13 20:00:51 -0500

    Update models.py - add cloudinaryField and import package - reorganize models and add slugify import

--- main/models.py
+++ main/models.py
@@ -1,16 +1,18 @@
 # main/models.py
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='project_images/')
+    slug = models.SlugField(unique=True, editable=False)
+    image = CloudinaryField("image", folder="project_covers", blank=True, null=True)
     description = models.TextField()
     case_study = models.TextField() # Will be URL when done
     github_url = models.URLField(blank=True, null=True)
     live_url = models.URLField(blank=True, null=True)
-    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
@@ -24,8 +26,12 @@ class Project(models.Model):
         super(Project, self).save(*args, **kwargs)
 
 class ProjectImage(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='project_images/')
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+    image = CloudinaryField("image", folder="case_study_images", blank=True, null=True)
     caption = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
