19553823cebf32db63263e3fbc3e0d7bf77eb350
Lamprocapnos <shoben2799@gmail.com>
2026-01-16 15:53:54 -0500

    update urls.py - delete django_browser_reload.urls from urlpatterns=[] becuase it has been uninstalled

--- ADS_Portfolio/urls.py
+++ ADS_Portfolio/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__reload__/', include('django_browser_reload.urls')),
     path('', include('main.urls', namespace='main'))
 ]
 
